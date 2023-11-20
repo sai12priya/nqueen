80651d1fb405f8c42cef0acfb66fa8c4ae8e0454
sai12priya <saipriyankasathelli@gmail.com>
2023-11-20 22:14:39 +0530

    Commiting fird

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,15 @@
+import "./Footer.css";
+
+export default function Footer() {
+    return (
+        <footer id="Footer">
+            <p>
+            Made by Sai Priyanka{" "}
+          <a href="https://www.linkedin.com/in/sai-priyanka-412378247/">
+            <img src="https://i.ibb.co/hYg8sm7/linked-In-PNG9.png" alt="logo" style={{ width: "25px", height: "25px", objectFit: "contain", verticalAlign: "middle",marginLeft:'5px' }} />
+          </a>
+                
+            </p>
+        </footer>
+    )
+}
